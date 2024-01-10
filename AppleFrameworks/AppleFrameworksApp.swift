4d7234b15e24ccc9f2525c0c653eba80fafe707e
Ahmed Nagy <ahmednaagy97@gmail.com>
2024-01-10 05:36:58 +0200

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Ahmed Nagy on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
